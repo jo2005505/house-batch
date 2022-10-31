35c4cdb631f07824919f0dc6f42ef58c3860a76c
Kraken <jo2005505@gmail.com>
2022-11-01 00:22:49 +0900

    Project Start Setting (호갱노노 프로젝트)
    프로젝트 목적
    - 아파트 실거래가 수집 배치 개발
    
    프로젝트 기본 셋팅
    - Spring Boot Version : 2.7.5
    - Java Version : 17
    - 의존성 설정
        - Lombok
        - Spring Configuration Processor
        - Spring Batch
        - Spring Data JPA
        - Spring Data JDBC
        - MySQL Driver
        - H2 Database

---
+++ src/test/java/com/housebatch/housebatch/HouseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housebatch.housebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
