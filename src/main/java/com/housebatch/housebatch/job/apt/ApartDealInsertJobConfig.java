69f35edeba6a4bf79452a4dc692729ac89e9ff32
Kraken <jo2005505@gmail.com>
2022-11-15 00:22:44 +0900

    호갱노노 프로젝트 - 아파트 실거래가 API 연결
    - 공공 데이터 포털에서 제공하는 아파트 실거래가 API 연동
    - Spring Batch 실행 파라미터 옵션
       lawdCd : 법정동 코드 앞 5자리
       yearMonth : 계약 년월

--- src/main/java/com/housebatch/housebatch/job/apt/ApartDealInsertJobConfig.java
+++ src/main/java/com/housebatch/housebatch/job/apt/ApartDealInsertJobConfig.java
@@ -1,15 +1,20 @@
 package com.housebatch.housebatch.job.apt;
 
+import com.housebatch.housebatch.adapter.ApartmentApiResource;
 import com.housebatch.housebatch.core.dto.AptDealDto;
 import com.housebatch.housebatch.job.validator.FilePathParameterValidator;
+import com.housebatch.housebatch.job.validator.LawdCdParameterValidator;
+import com.housebatch.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
@@ -20,6 +25,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+import java.time.YearMonth;
+import java.util.Arrays;
+
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -27,6 +35,7 @@ public class ApartDealInsertJobConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ApartmentApiResource apartmentApiResource;
 
     @Bean
     public Job aptDealInsertJob(
@@ -34,9 +43,24 @@ public class ApartDealInsertJobConfig {
     ) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
-                .validator(new FilePathParameterValidator())
+                //.validator(new FilePathParameterValidator())
+                .validator(aptDealJobParameterValidator())
                 .start(aptDealInsertStep)
                 .build();
+    }
+
+    /**
+     * Spring Batch에서 다수의 Class파일을 사용한 유효성 체크가 필요한 경우 CompositeJobParametersValidator로 배열에 담아 넘겨주는 기능을 구성한 Method이다.
+     * @return CompositeJobParametersValidator
+     */
+    private JobParametersValidator aptDealJobParameterValidator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(
+                new YearMonthParameterValidator(),
+                new LawdCdParameterValidator()
+        ));
+
+        return validator;
     }
 
     @Bean
@@ -55,12 +79,15 @@ public class ApartDealInsertJobConfig {
     @Bean
     @StepScope
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
-            @Value("#{jobParameters['filePath']}") String filePath
+            // @Value("#{jobParameters['filePath']}") String filePath,  // 테스트를 위해 Sample Filed 읽을 경우 사용
+            @Value("#{jobParameters['yearMonth']}") String yearMonth
+            , @Value("#{jobParameters['lawdCd']}") String lawdCd
             , Jaxb2Marshaller jaxb2Marshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>()
                 .name("aptDealResourceReader")
-                .resource(new ClassPathResource(filePath))  // Sample Filed을 읽어 정상동작되는지 코드 테스트용
+                //.resource(new ClassPathResource(filePath))  // Sample Filed을 읽어 정상동작되는지 코드 테스트용
+                .resource(apartmentApiResource.getResource(lawdCd, YearMonth.parse(yearMonth)))
                 .addFragmentRootElements("item")  // 읽을 Root Element name 설정
                 .unmarshaller(jaxb2Marshaller)    // 파일을 객체에 맵핑하는 설정
                 .build();
