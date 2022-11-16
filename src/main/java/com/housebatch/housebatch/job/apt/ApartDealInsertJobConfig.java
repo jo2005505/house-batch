2653bb6b59844300b6ec5905181e6a37d82ce15a
Kraken <jo2005505@gmail.com>
2022-11-17 01:00:21 +0900

    호갱노노 프로젝트 - 2번의 읽기 처리
    ** 테스트용으로 하나의 법정동 코드만 읽어오도록 처리 **
    
    [고려사항]
    ** 두 번의 '읽기' 처리 필요성 **
      - 구 코드 읽어오기
      - XML(아파트 실거래가) 파일 읽어오기 
    
    [해결방안]
    1. 하나의 스텝으로 구성
      1) XML 파싱 과정을 모두 작성 (로직이 복잡)
      2) XML 파일 읽기를 Chunk 단위로 작성 불가능 (대량의 데이터를 1번에 모두 처리하게 되는 문제)
    
    2. 두 개의 스텝으로 구성
      1) 구 코드를 읽는 Step과 API를 호출하여 XML을 읽는 스텝으로 구성
         a. Step 간의 데이터 전달은 ExecutionContext를 사용(https://docs.spring.io/spring-batch/docs/current/reference/html/index-single.html#executioncontext)
         b. Step 간 데이터를 전달하는 과정을 개발 필요
    
    
    ** ExecutionContext란?
    - Spring Batch MetaData Table에 내용을 저장하고, 참조하는 방식으로 동작 
    - 중단된 작업을 이어서 진행할 때 사용
    - Step 간의 데이터를 전달할 때 사용

--- src/main/java/com/housebatch/housebatch/job/apt/ApartDealInsertJobConfig.java
+++ src/main/java/com/housebatch/housebatch/job/apt/ApartDealInsertJobConfig.java
@@ -8,10 +8,7 @@ import com.housebatch.housebatch.job.validator.LawdCdParameterValidator;
 import com.housebatch.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersValidator;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -20,6 +17,7 @@ import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
@@ -32,6 +30,7 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -46,13 +45,16 @@ public class ApartDealInsertJobConfig {
     @Bean
     public Job aptDealInsertJob(
             Step guLawdCdStep
-//            Step aptDealInsertStep
+            , Step contextPrintStep
+            , Step aptDealInsertStep
     ) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
                 //.validator(new FilePathParameterValidator())
-                //.validator(aptDealJobParameterValidator())
+                .validator(aptDealJobParameterValidator())
                 .start(guLawdCdStep)
+                .next(contextPrintStep)
+                .next(aptDealInsertStep)
                 .build();
     }
 
@@ -63,8 +65,8 @@ public class ApartDealInsertJobConfig {
     private JobParametersValidator aptDealJobParameterValidator() {
         CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
         validator.setValidators(Arrays.asList(
-                new YearMonthParameterValidator(),
-                new LawdCdParameterValidator()
+                new YearMonthParameterValidator()
+                //, new LawdCdParameterValidator()
         ));
 
         return validator;
@@ -88,10 +90,47 @@ public class ApartDealInsertJobConfig {
     @StepScope
     public Tasklet guLawdCdTasklet() {
         return (contribution, chunkContext) -> {
-            lawdRepository.findDistinctGuLawdCd()
-                    .forEach(System.out::println);
+            /*
+             * ExecutionContext 사용
+             * 사용하는 경우
+             *   1. 중단된 배치 작업을 이어서 진행하기 위한 경우
+             *   2. Step 간 데이터를 전달이 필요한 경우
+             * 
+             * 동작 방식
+             *   - Spring Batch 실행내역을 Batch 관련 테이블에 메타데이터로 저장
+             *     (데이터베이스 테이블 명 : BATCH_JOB_EXECUTION_CONTEXT)
+             *   - Spring Batch 메타데이터 테이블을 참조하여 필요 데이터 조회
+             */
+            StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
+            ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
+
+            List<String> guLawdCds = lawdRepository.findDistinctGuLawdCd();
+            executionContext.putString("guLawdCd", guLawdCds.get(0));
+
             return RepeatStatus.FINISHED;
         };
+    }
+
+    @Bean
+    @JobScope
+    public Step contextPrintStep(
+            Tasklet contextPrintTasklet
+    ) {
+        return stepBuilderFactory.get("contextPrintStep")
+                .tasklet(contextPrintTasklet)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet contextPrintTasklet(
+            @Value("#{jobExecutionContext['guLawdCd']}") String guLawdCd    // jobExecutionContext로 메타데이터 테이블에서 조회 가능
+    ) {
+        return ((contribution, chunkContext) -> {
+            System.out.println("[contextPrintStep] guLawdCd ::: " + guLawdCd);
+
+            return RepeatStatus.FINISHED;
+        });
     }
 
     @Bean
@@ -112,13 +151,14 @@ public class ApartDealInsertJobConfig {
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
             // @Value("#{jobParameters['filePath']}") String filePath,  // 테스트를 위해 Sample Filed 읽을 경우 사용
             @Value("#{jobParameters['yearMonth']}") String yearMonth
-            , @Value("#{jobParameters['lawdCd']}") String lawdCd
+            //, @Value("#{jobParameters['lawdCd']}") String lawdCd      // Spring Batch 실행 시 입력된 파라미터를 사용
+            , @Value("#{jobExecutionContext['guLawdCd']}") String guLawdCd  // ExecutionContext 데이터베이스에 저장되어있는 메타데이터를 사용
             , Jaxb2Marshaller jaxb2Marshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>()
                 .name("aptDealResourceReader")
                 //.resource(new ClassPathResource(filePath))  // Sample Filed을 읽어 정상동작되는지 코드 테스트용
-                .resource(apartmentApiResource.getResource(lawdCd, YearMonth.parse(yearMonth)))
+                .resource(apartmentApiResource.getResource(guLawdCd, YearMonth.parse(yearMonth)))
                 .addFragmentRootElements("item")  // 읽을 Root Element name 설정
                 .unmarshaller(jaxb2Marshaller)    // 파일을 객체에 맵핑하는 설정
                 .build();
